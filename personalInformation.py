ab3308ecfeec431d93b9c1cc5659d4f7a6ab90b4
cabaelamirxle <amirxlecabael28@gmail.com>
2022-11-12 23:39:57 +0800

    ask user input

--- personalInformation.py
+++ personalInformation.py
@@ -1,6 +1,18 @@
 #declare empty directory
+personalInfo = {
+
+}
 #display menu
+print ("==========Menu==========")
+print ("")
+print ("1. Add an item")
+print ("2. Search")
+print ("Exit Y/N")
+print ("")
+print ("========================")
+
 #ask user input
+personalData = int(input("What do you want to do? (1-3): "))
 #if user input 1: ask personal data
 #if user input 2: ask full name
 #if user input 3: ask if want to exit
